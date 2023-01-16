43e2fe16ba4e5b5626309448b28a43625df7cae1
Cristhian Bonilla <cristhian.espinosa@dowjones.com>
2023-01-16 13:21:32 -0500

    test httpLCient

--- src/main/kotlin/optimusfly/plugins/Gpt3Module.kt
+++ src/main/kotlin/optimusfly/plugins/Gpt3Module.kt
@@ -2,13 +2,13 @@ package optimusfly.plugins
 
 import io.ktor.client.*
 import io.ktor.client.request.*
-import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.coroutines.runBlocking
 import optimusfly.domain.model.gpt.GptRequest
 
 fun Application.gpt3Module() {
@@ -17,8 +17,13 @@ fun Application.gpt3Module() {
             post("get-sermons") {
                 val request = call.receive<GptRequest>()
                //  val client = HttpClient()
-                val response: HttpResponse = HttpClient().request("https://ktor.io/") {
-                    method = HttpMethod.Get
+                runBlocking {
+// Sample for making a HTTP Client request
+                    val message = HttpClient().post { // or your data class
+                        url("url")
+                        this.contentType(ContentType.Application.Json)
+                        this.setBody(request)
+                    }
                 }
                 // Establecer la URL de la API de GPT-3 y la clave de API
                 val apiUrl = "https://api.openai.com/v1/completions"
