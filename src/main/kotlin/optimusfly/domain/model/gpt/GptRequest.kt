224c3ebb4c917a32306f59208a00526f16dae5d6
Cristhian Bonilla <cristhian.espinosa@dowjones.com>
2023-01-16 12:36:26 -0500

    probar request

--- src/main/kotlin/optimusfly/domain/model/gpt/GptRequest.kt
+++ src/main/kotlin/optimusfly/domain/model/gpt/GptRequest.kt
@@ -4,13 +4,14 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class GptRequest(
-    val frequencyPenalty: Double?,
-    val max_tokens: Int?,
-    val model: String?,
-    val presence_penalty: Double?,
-    val prompt: String? = null,
-    val stop: List<String>?,
-    val temperature: Double?,
-    val top_p: Int?,
-    val user: String?
+    val model: String,
+    val prompt: String,
+    val max_tokens: Int,
+    val temperature: Double,
+    val top_p: Int,
+    val n: Int,
+    val stream: Boolean,
+    val logprobs: List<String>?,
+    val stop: List<String>,
+    val user: String
 )
--- src/main/kotlin/optimusfly/plugins/Gpt3Module.kt
+++ src/main/kotlin/optimusfly/plugins/Gpt3Module.kt
@@ -1,19 +1,13 @@
 package optimusfly.plugins
 
 import io.ktor.client.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.util.*
 import optimusfly.domain.model.gpt.GptRequest
-import optimusfly.domain.model.user.UserResponse
 
-@OptIn(InternalAPI::class)
 fun Application.gpt3Module() {
     routing {
         authenticate {
@@ -28,6 +22,4 @@ fun Application.gpt3Module() {
             }
         }
     }
-
-
 }
