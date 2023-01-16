c7ea6ccfce787650bd2c018f4616cd717ae7d899
Cristhian Bonilla <cristhian.espinosa@dowjones.com>
2023-01-16 12:04:53 -0500

    refactor gptRequest

--- src/main/kotlin/optimusfly/domain/model/gpt/GptRequest.kt
+++ src/main/kotlin/optimusfly/domain/model/gpt/GptRequest.kt
@@ -4,13 +4,13 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class GptRequest(
-    val frequencyPenalty: Double,
-    val maxTokens: Int,
-    val model: String,
-    val presencePenalty: Double,
+    val frequencyPenalty: Double?,
+    val max_tokens: Int?,
+    val model: String?,
+    val presence_penalty: Double?,
     val prompt: String? = null,
-    val stop: List<String>,
-    val temperature: Double,
-    val topP: Int,
-    val user: String
+    val stop: List<String>?,
+    val temperature: Double?,
+    val top_p: Int?,
+    val user: String?
 )
