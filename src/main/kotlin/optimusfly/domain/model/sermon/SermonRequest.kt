18a116243f74d414f5b6f0920b2a3f487ed63b4f
Cristhian Bonilla <cristhian.espinosa@dowjones.com>
2023-01-16 17:08:37 -0500

    modify request sermon

--- src/main/kotlin/optimusfly/domain/model/sermon/SermonRequest.kt
+++ src/main/kotlin/optimusfly/domain/model/sermon/SermonRequest.kt
@@ -4,8 +4,6 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class SermonRequest(
-    val userId: Int?,
-    val userName: String?,
     val sermon: String,
     val category: Int?
 )
