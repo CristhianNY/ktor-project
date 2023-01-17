45bac9a7cf6edbafa2e76fcf166659b609308c70
Cristhian Bonilla <cristhian.espinosa@dowjones.com>
2023-01-16 19:20:51 -0500

    sermon category

--- src/main/kotlin/optimusfly/data/sermons/CategoryEntity.kt
+++ src/main/kotlin/optimusfly/data/sermons/CategoryEntity.kt
@@ -4,7 +4,7 @@ import org.ktorm.schema.Table
 import org.ktorm.schema.int
 import org.ktorm.schema.text
 
-object CategoryEntity: Table<Nothing>("Category") {
+object CategoryEntity: Table<Nothing>("sermon_category") {
     val id = int("idCategory").primaryKey()
     val name  = text("name")
 }
