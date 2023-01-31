f193e2a360550687c1817a399bf254e664af0c7c
Cristhian Bonilla <cristhian.espinosa@dowjones.com>
2023-01-31 07:56:04 -0500

    json kotlin

--- src/main/kotlin/optimusfly/data/openai/OpenAi.kt
+++ src/main/kotlin/optimusfly/data/openai/OpenAi.kt
@@ -10,19 +10,22 @@ class OpenAI(apiKey: String) {
 
     fun completion(prompt: String, maxTokens: Int): Response {
 
-        val requestBody = "{\n" +
-                "  \"model\": \"text-davinci-003\",\n" +
-                "  \"prompt\": \"You: What have you been up to?\\nFriend: Watching old movies.\\nYou: Did you watch anything interesting?\\nFriend:\",\n" +
-                "  \"temperature\": 0.5,\n" +
-                "  \"max_tokens\": 60,\n" +
-                "  \"top_p\": 1.0,\n" +
-                "  \"frequency_penalty\": 0.5,\n" +
-                "  \"presence_penalty\": 0.0,\n" +
-                "  \"stop\": [\"You:\"]\n" +
-                "}"
+        val requestBody = """
+{
+  "model": "text-davinci-003",
+  "prompt": "You: What have you been up to?\nFriend: Watching old movies.\nYou: Did you watch anything interesting?\nFriend:",
+  "temperature": 0.5,
+  "max_tokens": 60,
+  "top_p": 1.0,
+  "frequency_penalty": 0.5,
+  "presence_penalty": 0.0,
+  "stop": ["You:"]
+}
+"""
         val request = Request.Builder()
             .url("https://api.openai.com/v1/engines/curie/completions")
-            .post(requestBody.trimIndent().toRequestBody("application/json".toMediaType())
+            .post(
+                requestBody.trimIndent().toRequestBody("application/json".toMediaType())
             )
             .addHeader("Authorization", "Bearer $apiKey")
             .build()
