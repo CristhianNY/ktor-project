ac711786701f43601c23125169e9ea6426479aa3
Cristhian Bonilla <cristhian.espinosa@dowjones.com>
2023-02-03 11:43:23 -0500

    update password

--- src/main/kotlin/optimusfly/data/db/DatabaseConnection.kt
+++ src/main/kotlin/optimusfly/data/db/DatabaseConnection.kt
@@ -6,5 +6,5 @@ object DatabaseConnection {
     val database = Database.connect(url = "jdbc:mysql://pxukqohrckdfo4ty.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/fchie35hl9k8xp42",
         driver = "com.mysql.cj.jdbc.Driver",
         user = "ajrccsdluf760l8a",
-        password = "in7ejte8rftnwlky")
+        password = "gib6994vbm70f5jx")
 }
