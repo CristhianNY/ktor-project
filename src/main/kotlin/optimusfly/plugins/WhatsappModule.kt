70f9d33068a7b07e8177c2d13c4214b5c6e95043
Cristhian Bonilla <cristhian.espinosa@dowjones.com>
2023-02-05 20:47:12 -0500

    token

--- src/main/kotlin/optimusfly/plugins/WhatsappModule.kt
+++ src/main/kotlin/optimusfly/plugins/WhatsappModule.kt
@@ -71,7 +71,7 @@ fun Application.whatsappModule() {
 
             if (message <= 1) {
                 val whatsAppApi =
-                    WhatsAppApi("2EAAMZBu7GdAScBAKGOkhaVZA9FueJWQupu72vL4GMSrEZA4NoRerYGaecbAMOUpzDaDreTyRShZCwNS26UHJC8ExxnKgdEZASZBk9xGOmPRm38WJtuMxwPtd5zognI1ls8kBVtN1KmgnAMGNdkcyXM0nEyfllZBRRp3H7KodPb7YZBzlhjOb2bZABMY14OV6XqUVrjGE0xFOxLFK4YQamIYu3k")
+                    WhatsAppApi("EAAMZBu7GdAScBAKGOkhaVZA9FueJWQupu72vL4GMSrEZA4NoRerYGaecbAMOUpzDaDreTyRShZCwNS26UHJC8ExxnKgdEZASZBk9xGOmPRm38WJtuMxwPtd5zognI1ls8kBVtN1KmgnAMGNdkcyXM0nEyfllZBRRp3H7KodPb7YZBzlhjOb2bZABMY14OV6XqUVrjGE0xFOxLFK4YQamIYu3k")
 
                 launch(Dispatchers.IO) {
                     val response = whatsAppApi.sendMessage(
