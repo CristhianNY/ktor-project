0bb9feedb04fad7b2ee57459ac6f011808730e0e
cristhian <cristhian.espinosa@dowjones.com>
2023-03-03 09:52:17 -0500

    updated token whatsapp

--- src/main/kotlin/optimusfly/data/whatsappApi/WhatsAppApi.kt
+++ src/main/kotlin/optimusfly/data/whatsappApi/WhatsAppApi.kt
@@ -7,7 +7,7 @@ import okhttp3.RequestBody
 import okhttp3.Response
 import optimusfly.domain.model.whatsapp.send.MessageToSendModel
 
-class WhatsAppApi(val bearerToken: String) {
+class WhatsAppApi {
     private val client = OkHttpClient()
 
     fun sendMessage(message: MessageToSendModel): Response {
@@ -19,7 +19,7 @@ class WhatsAppApi(val bearerToken: String) {
             .url("https://graph.facebook.com/v15.0/116126038051629/messages")
             .post(RequestBody.create(mediaType, body))
             .addHeader("Content-Type", "application/json")
-            .addHeader("Authorization", "Bearer $bearerToken")
+            .addHeader("Authorization", "Bearer EAAMZBu7GdAScBAJRRCDnsZCcJAoYvh4r8lObLf2wxdtpzpQUOMhSswe7o6Ng2HfZBH3ZBAQzWK9RsTQmkQXWtlrNo3jZC2FAIOIluyJymmUcYDNqkMkOp2z74qZCm5pjIf4Y06c3k1Jq7RLAu8UYk4bUz0B8kxYoJSMRUqimHZB4VOD5gyZAwX19rilviEc0RKfJLBrYJ60RDLVR3u2sJ9PA")
             .build()
 
         client.newCall(request).execute().use { response ->
--- src/main/kotlin/optimusfly/plugins/WhatsappModule.kt
+++ src/main/kotlin/optimusfly/plugins/WhatsappModule.kt
@@ -64,10 +64,7 @@ fun Application.whatsappModule() {
 
 
             if (request2.entry?.first()?.changes?.first()?.value?.statuses?.first() == null) {
-                val whatsAppApi =
-                    WhatsAppApi("EAAMZBu7GdAScBABzWKROdNjjD9Tp1VX2VntNrUZBlbCktz8BLCp8yEbmaZAzA1ZARL6SW3Vwoq650hqnrYZC6JHAL8LaqxqLCER6b5UwqyCNhVzd2lMDLUxZBJCJoZCEZCN75ZCYHd9GIDW88Om9SpZAfx2u4DkoI9SXJqv3gTkjqnuZBkLK1YZAzaqibXvR9WrfpdtVjhua0lVDo35etXFwsv63")
-
-
+                val whatsAppApi = WhatsAppApi()
 
                 launch(Dispatchers.IO) {
                     val gptResponse = async {
@@ -129,8 +126,7 @@ fun Application.whatsappModule() {
                 )
             )
 
-            val whatsAppApi =
-                WhatsAppApi("EAAMZBu7GdAScBACJg0zZACLHZC5eTzT1rs1ihGFe7PjUKHSfZCk1l1S7R4AQRIsHmXR3UN8vUqFyXj9H3ztk47JMz5mdXqBntJgjkEG8EbRA1hBdQw5qLihMInulMFqYZBbOq9yCyRMqZBO35mGWGm6t4vEjucFQ3HrBjBZBqZA67pT9QmP5BUVCaz8QVKmHpIA2vGA5vqaJV9izZCeMkWGTQ")
+            val whatsAppApi = WhatsAppApi()
 
             launch(Dispatchers.IO) {
                 val response = whatsAppApi.sendMessage(
